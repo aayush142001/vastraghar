2c27cb2295c9ba907765a2681ab5c21cf4e45620
Aayush <aayush.142001@gmail.com>
2024-02-26 17:44:49 +0530

    first commit

---
+++ src/main/java/com/vastraghar/VastragharApplication.java
@@ -0,0 +1,13 @@
+package com.vastraghar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VastragharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VastragharApplication.class, args);
+	}
+
+}
